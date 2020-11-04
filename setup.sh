e792f001591c12ccac7c47a5a1ec1d8fa1c305f9
Brandon Atkinson <bj.atkinson92@gmail.com>
2020-11-04 16:20:00 -0700

    remove dead code

--- setup.sh
+++ setup.sh
@@ -1,20 +1,2 @@
 npm init -y
 npm install
-
-# Webpack
-# npm install webpack webpack-cli --save-dev
-# npm install --save-dev mini-css-extract-plugin
-# npm i optimize-css-assets-webpack-plugin
-
-# CSS
-# npm install --save-dev postcss@^8.1.0
-# npm install sass-loader sass webpack --save-dev
-# npm install --save-dev style-loader
-# npm install --save-dev css-loader
-
-
-# Svelte
-# npm install --save svelte svelte-loader
-
-# SMUI installs
-# npm install --save-dev @smui/button
